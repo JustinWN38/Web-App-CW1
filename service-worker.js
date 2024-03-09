5084708d741d00136f7e9fadc8b2b714a5d94460
JustinWN38 <justin@wells-neczyporczuk.co.uk>
2024-03-09 12:45:27 +0000

    making changes

--- service-worker.js
+++ service-worker.js
@@ -1,40 +1,46 @@
 var cacheName = "LessonShop";
 var cacheFiles = [
- "index.html",
- //"products.js",
- "Images/Networking.png",
- "Images/Neurology.png",
- "Images/Physics.png",
- "Images/Biology.png",
- "Images/Chemistry.png",
- "Images/Computer.png",
- "Images/Drama.png",
- "Images/English.png",
- "Images/Games.png",
- "Images/Maths.png",
+  "index.html",
+  // "products.js", // Uncomment this line if you have a products.js file
+  "Images/Networking.png",
+  "Images/Neurology.png",
+  "Images/Physics.png",
+  "Images/Biology.png",
+  "Images/Chemistry.png",
+  "Images/Computer.png",
+  "Images/Drama.png",
+  "Images/English.png",
+  "Images/Games.png",
+  "Images/Maths.png",
 ];
 
-self.addEventListener("install", function(e) {
+self.addEventListener("install", function (e) {
   console.log("[Service Worker] Install");
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(cacheName).then(function (cache) {
       console.log("[Service Worker] Caching files");
       return cache.addAll(cacheFiles);
     })
   );
 });
 
-self.addEventListener("fetch", function(e) {
+self.addEventListener("fetch", function (e) {
   e.respondWith(
-    caches.match(e.request).then(function(r) {
+    caches.match(e.request).then(function (r) {
       console.log("[Service Worker] Fetching resource: " + e.request.url);
-      return r || fetch(e.request).then(function(response) {
-        return caches.open(cacheName).then(function(cache) {
-          cache.put(e.request, response.clone());
-          console.log("[Service Worker] Resource fetched and saved in the cache for: " + e.request.url);
-          return response;
-        });
-      });
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            console.log(
+              "[Service Worker] Resource fetched and saved in the cache for: " +
+                e.request.url
+            );
+            return response;
+          });
+        })
+      );
     })
   );
 });
