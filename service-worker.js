1377fed4185f412a37a19320cfbfea798cc204c4
JustinWN38 <justin@wells-neczyporczuk.co.uk>
2024-03-09 14:32:39 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,3 @@
-const staticLessonShop = "Lesson Shop"
 var cacheName = "LessonShop";
 var cacheFiles = [
   "index.html",
@@ -15,27 +14,33 @@ var cacheFiles = [
   "Images/Maths.png",
 ];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticLessonShop).then(cache => {
-      cache.addAll(assets)
+self.addEventListener("install", function (e) {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      console.log("[Service Worker] Caching files");
+      return cache.addAll(cacheFiles);
     })
-  )
+  );
 });
 
-self.addEventListener("fetch", fetchEvent => {
-	fetchEvent.respondWith(
-	  caches.match(fetchEvent.request).then(res => {
-		return res || fetch(fetchEvent.request)
-	  })
-	)
-  });
-
-  if ("serviceWorker" in navigator) {
-	window.addEventListener("load", function() {
-	  navigator.serviceWorker
-		.register("/serviceWorker.js")
-		.then(res => console.log("service worker registered"))
-		.catch(err => console.log("service worker not registered", err))
-	})
-  }
+self.addEventListener("fetch", function (e) {
+  e.respondWith(
+    caches.match(e.request).then(function (r) {
+      console.log("[Service Worker] Fetching resource: " + e.request.url);
+      return (
+        r ||
+        fetch(e.request).then(function (response) {
+          return caches.open(cacheName).then(function (cache) {
+            cache.put(e.request, response.clone());
+            console.log(
+              "[Service Worker] Resource fetched and saved in the cache for: " +
+                e.request.url
+            );
+            return response;
+          });
+        })
+      );
+    })
+  );
+});
