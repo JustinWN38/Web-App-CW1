726c3d5e71b36779e52a818e388a50b35b9d9b2d
JustinWN38 <justin@wells-neczyporczuk.co.uk>
2024-03-09 15:13:45 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -16,35 +16,20 @@ var cacheFiles = [
 self.addEventListener("install", function (e) {
 	console.log("[Service Worker] Install");
 	e.waitUntil(
-	  caches.open(cacheName).then(function (cache) {
-		console.log("[Service Worker] Caching files");
-		return cache.addAll(cacheFiles);
-	  })
+		caches.open(cacheName).then(function (cache) {
+			console.log("[Service Worker] Caching files");
+			return cache.addAll(cacheFiles);
+		})
 	);
-  });
-  
-  self.addEventListener("fetch", function (e) {
+});
+
+self.addEventListener('fetch', function (e) {
 	e.respondWith(
-	  caches.match(e.request).then(function (r) {
-		console.log("[Service Worker] Fetching resource: " + e.request.url);
-		return (
-		  r ||
-		  fetch(e.request).then(function (response) {
-			if (!response || response.status !== 200 || response.type !== 'basic') {
-			  console.error("[Service Worker] Fetch failed for: " + e.request.url);
-			  return response;
-			}
-  
-			return caches.open(cacheName).then(function (cache) {
-			  console.log(
-				"[Service Worker] Resource fetched and saved in the cache for: " +
-				  e.request.url
-			  );
-			  cache.put(e.request, response.clone());
-			  return response;
-			});
-		  })
-		);
-	  })
+		// check if the cache has the file
+		caches.match(e.request).then(function (r) {
+			console.log('[Service Worker] Fetching resource: ‘ + e.request.url);
+	// 'r' is the matching file if it exists in the cache
+	return r
+		})
 	);
-  });
+});
