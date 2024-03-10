ebcf960c127879e65e2eb036ffbee6d68c3e874e
JustinWN38 <justin@wells-neczyporczuk.co.uk>
2024-03-10 18:10:47 +0000

    Changes to PWA

--- service-worker.js
+++ service-worker.js
@@ -12,7 +12,8 @@ var cacheFiles = [
   "Images/Games.png",
   "Images/Maths.png",
   "Images/icon1.png",
-  "Images/icon2.png"
+  "Images/icon2.png",
+  "data.json"
 ];
 
 self.addEventListener("install", function (e) {
@@ -24,36 +25,31 @@ self.addEventListener("install", function (e) {
 		})
 	);
 });
-/*
-self.addEventListener('fetch', function (e) {
-	e.respondWith(
-		// check if the cache has the file
-		caches.match(e.request).then(function (r) {
-			console.log('[Service Worker] Fetching resource: ' + e.request.url);
-	// 'r' is the matching file if it exists in the cache
-	return r
-		})
-	);
-});
-*/
+
 self.addEventListener("fetch", function (e) {
-	e.respondWith(
-		caches.match(e.request).then(function (cachedFile) {
-			//if the file is in the cache, retrieve it from there
-			if (cachedFile) {
-				console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
-				return cachedFile;
-			} else {//if the file is not in the cache, download the file
-				return fetch(e.request).then(function (response) {
-					return caches.open(cacheName).then(function (cache) {
-						//add the new file to the cache
-						cache.put(e.request, response.clone());
-						console.log("[Service Worker] Resource fetched and saved in the cache for: " +
-							e.request.url);
-						return response;
-					});
-				});
-			}
-		})
-	);
+    e.respondWith(
+        caches.match(e.request).then(function (cachedFile) {
+            // If the file is in the cache, retrieve it from there
+            if (cachedFile) {
+                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+                return cachedFile;
+            } else {
+                // If the file is not in the cache, download the file
+                return fetch(e.request).then(function (response) {
+                    return caches.open(cacheName).then(function (cache) {
+                        // Add the new file to the cache
+                        cache.put(e.request, response.clone());
+                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
+                            e.request.url);
+                        return response;
+                    });
+                }).catch(function (error) {
+                    console.error("[Service Worker] Fetch failed:", error);
+                    // Optionally, return a custom offline page or handle the error gracefully
+                    // You can customize this based on your application's needs
+                    return caches.match('offline.html'); // Make sure you have an 'offline.html' in your cache
+                });
+            }
+        })
+    );
 });
